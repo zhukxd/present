4a2ac98bf6ff8ed5a0ac45f8fb832b3b4ab14fa4
zhukxd <123548574+zhukxd@users.noreply.github.com>
2026-03-08 22:29:07 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Это наилучшее пожелание'.split('')
+const text = 'Скромное пожелание на праздник'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
